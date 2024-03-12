78a11af8081d8d94dc93a4613defb798c75efc68
thekvector <74088461+thekvector@users.noreply.github.com>
2024-03-12 00:41:49 -0400

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,7 +1,7 @@
 import tweepy
 import json
 
-
+#Get all these authorizations from the twitter developer account
 api_key = 'YOUR_API_KEY'
 api_key_secret = 'YOUR_API_SECRET'
 access_token = 'YOUR_ACCESS_TOKEN'
@@ -11,8 +11,10 @@ auth = tweepy.OAuthHandler(api_key, api_key_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
+#This is what we need to work on. figure out what search words and dates.
 search_words = "#examplehashtag"
 date_since = "2021-01-01"
+
 
 tweets = tweepy.Cursor(api.search_tweets,
               q=search_words,
