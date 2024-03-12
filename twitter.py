e6c72c892ad4aa8b4528cc02abc6f400f95a05cb
thekvector <74088461+thekvector@users.noreply.github.com>
2024-03-12 00:39:32 -0400

    Add files via upload

---
+++ twitter.py
@@ -0,0 +1,29 @@
+import tweepy
+import json
+
+
+api_key = 'YOUR_API_KEY'
+api_key_secret = 'YOUR_API_SECRET'
+access_token = 'YOUR_ACCESS_TOKEN'
+access_token_secret = 'YOUR_ACCESS_TOKEN_SECRET'
+
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+search_words = "#examplehashtag"
+date_since = "2021-01-01"
+
+tweets = tweepy.Cursor(api.search_tweets,
+              q=search_words,
+              lang="en",
+              since=date_since).items(5)
+
+tweets_data = []
+for tweet in tweets:
+    tweets_data.append(tweet._json)  
+
+with open('tweets.json', 'w') as outfile:
+    json.dump(tweets_data, outfile, indent=4)
+
+print("Tweets have been saved to tweets.json")
